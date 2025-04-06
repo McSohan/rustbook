254fa645e601ff9a1c0767a0ca9dae5724769dea
McSohan <mcsohan6300@gmail.com>
2025-04-05 22:00:34 -0700

    remove unwanted files from commit

--- variables/target/debug/deps/variables.d
+++
@@ -1,5 +0,0 @@
-C:\Users\mcsoh\Documents\UCSD\Distributed systems\Rustbook\rustbook\variables\target\debug\deps\variables.exe: src\main.rs
-
-C:\Users\mcsoh\Documents\UCSD\Distributed systems\Rustbook\rustbook\variables\target\debug\deps\variables.d: src\main.rs
-
-src\main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-C:\Users\mcsoh\Documents\UCSD\Distributed\ systems\Rustbook\rustbook\variables\target\debug\variables.exe: C:\Users\mcsoh\Documents\UCSD\Distributed\ systems\Rustbook\rustbook\variables\src\main.rs
